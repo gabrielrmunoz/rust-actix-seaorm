5b7365bbd5999f27ba8a78a4fc712e868c8325a7
Gabriel Ruiz Muñoz <gabriel.rmunoz.coding@gmail.com>
2025-03-19 10:30:17 +0100

    chore: migrate from route to service

--- src/routes.rs
+++ src/routes.rs
@@ -5,21 +5,18 @@ use sea_orm::DbConn;
 pub fn configure_routes(cfg: &mut web::ServiceConfig, db: DbConn) {
     cfg.app_data(web::Data::new(db)).service(
         web::scope("/api")
-            .route("/users", web::get().to(handlers::get_users))
-            .route("/users/{id}", web::get().to(handlers::get_user))
-            .route("/users", web::post().to(handlers::create_user))
-            .route("/users/{id}", web::put().to(handlers::update_user))
-            .route(
-                "/users/{id}",
-                web::delete().to(handlers::delete_user_physical),
+            .service(
+                web::resource("/users")
+                    .get(handlers::get_users)
+                    .post(handlers::create_user),
             )
-            .route(
-                "/users/{id}/soft-delete",
-                web::patch().to(handlers::delete_user_logical),
+            .service(
+                web::resource("/users/{id}")
+                    .get(handlers::get_user)
+                    .put(handlers::update_user)
+                    .delete(handlers::delete_user_physical),
             )
-            .route(
-                "/users/{id}/restore",
-                web::patch().to(handlers::restore_user),
-            ),
+            .service(web::resource("/users/{id}/soft-delete").patch(handlers::delete_user_logical))
+            .service(web::resource("/users/{id}/restore").patch(handlers::restore_user)),
     );
 }
